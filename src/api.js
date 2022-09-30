746c7ac7d71399ae7fab73194330ee5a8028174e
InvisibleHawk <minkinrd@gmail.com>
2022-10-01 09:04:42 +1000

    add websokets

--- src/api.js
+++ src/api.js
@@ -1,33 +1,63 @@
 const API_KEY = 'c409232238d3508710a5599f4226424615c6dd491b874d3cdbf4755ece7348af'
+const AGGREGATE_INDEX = '5';
 
 const tickersHandlers = new Map()
 
-export const loadTicker = () => {
+const socket = new WebSocket(`wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`)
 
-    if(tickersHandlers.size === 0 ) return
+socket.addEventListener('message', (e) => {
+    const { TYPE: type, FROMSYMBOL: currency, PRICE: newPrice }  = JSON.parse(e.data)
+    if(type !== AGGREGATE_INDEX || newPrice === undefined) {
+        return
+    }
+    const handlers = tickersHandlers.get(currency) ?? []
+    handlers.forEach(fn => fn(newPrice))
+})
 
-    fetch(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${[...tickersHandlers.keys()].join(',')}&tsyms=USD&api_key=${API_KEY}`)
-    .then(r => r.json())
-    .then(rawData =>  {
-        const updatedPrices = Object.fromEntries(Object.entries(rawData).map(([key, value]) => [key, value.USD]))
-        Object.entries(updatedPrices).forEach(([currency, newPrice]) => {
-            const handlers = tickersHandlers.get(currency) ?? []
-            handlers.forEach(fn => fn(newPrice))
-        })
-    })
+function sendToWS(message) {
+    const strMessage = JSON.stringify(message)
+    if(socket.readyState === socket.OPEN)  {
+        socket.send(strMessage)
+        return
+    }
+
+    socket.addEventListener('open', () => {
+        socket.send(strMessage)
+    }, {once: true})  
+}
+
+function subscribeToTickerOnWS(tickerName) {
+    const message = {
+        action: 'SubAdd',
+        subs: [`5~CCCAGG~${tickerName}~USD`]
+    }
+
+    sendToWS(message)
+}
+
+function unsubscribeFromTickerOnWS(tickerName) {
+    const message = {
+        action: 'SubRemove',
+        subs: [`5~CCCAGG~${tickerName}~USD`]
+    }
+
+    sendToWS(message)
 }
 
 export const subscribeToTicker = (tickerName, cb) => {
     const subscribes = tickersHandlers.get(tickerName) || []
     tickersHandlers.set(tickerName, [...subscribes, cb])
+    subscribeToTickerOnWS(tickerName)
 }
 
 export const unsubscribeFromTicker = (tickerName) => {
     console.log(`tickerName handler for ${tickerName} was delete`)
     tickersHandlers.delete(tickerName)
+    unsubscribeFromTickerOnWS(tickerName)
 }
 
-
-setInterval(loadTicker, 5000)
+export const getTickersList = () => 
+    fetch(`https://min-api.cryptocompare.com/data/all/coinlist?summary=true`)
+    .then(r => r.json())
 
 window.tickersHandlers = tickersHandlers
